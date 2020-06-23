64834058b2fce1e0da1c581e52ae38bc22647387
Maxim Zelenkin <nudepatch@gmail.com>
2020-06-23 06:00:03 +0500

    DRY tests

--- test/models/checkout_test.rb
+++ test/models/checkout_test.rb
@@ -1,29 +1,29 @@
 require 'test_helper'
 
 class CheckoutTest < ActiveSupport::TestCase
+  def setup
+    @item_a = items(:a)
+    @rule_a = Rule.new(item_name: "A", batch_size: 3, batch_price: 75)
+  end
+
   test "should scan item and return price without rules (30 for A item)" do
-    item = items(:a)
     co = Checkout.new
-    co.scan(item)
+    co.scan(@item_a)
     assert_equal(30, co.total)
   end
 
   test "should return 75 for three A items" do
-    item = items(:a)
-    rule = Rule.new(item_name: "A", batch_size: 3, batch_price: 75)
-    co = Checkout.new([rule])
+    co = Checkout.new([@rule_a])
     3.times do
-      co.scan(item)
+      co.scan(@item_a)
     end
     assert_equal(75, co.total)
   end
 
   test "should return 105 for four A items" do
-    item = items(:a)
-    rule = Rule.new(item_name: "A", batch_size: 3, batch_price: 75)
-    co = Checkout.new([rule])
+    co = Checkout.new([@rule_a])
     4.times do
-      co.scan(item)
+      co.scan(@item_a)
     end
     assert_equal(105, co.total)
   end
